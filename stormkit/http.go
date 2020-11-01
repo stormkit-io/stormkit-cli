c9b2e371868997387c104b77be74ea2248ed7738
giuliobosco <giuliobva@gmail.com>
2020-11-01 18:22:03 +0100

    add stormkit/ http methods

--- stormkit/http.go
+++ stormkit/http.go
@@ -43,3 +43,23 @@ func request(m, api string, body io.Reader) (*http.Request, error) {
 
 	return r, nil
 }
+
+// Get create a get request for stormkit
+func Get(api string) (*http.Request, error) {
+	return request(http.MethodGet, api, nil)
+}
+
+// Post create a post request for stormkit
+func Post(a string, b io.Reader) (*http.Request, error) {
+	return request(http.MethodPost, a, b)
+}
+
+// Put create a put request for stormkit
+func Put(a string, b io.Reader) (*http.Request, error) {
+	return request(http.MethodPut, a, b)
+}
+
+// Delete create a delete request for stormkit
+func Delete(a string, b io.Reader) (*http.Request, error) {
+	return request(http.MethodDelete, a, b)
+}
--- stormkit/http_test.go
+++ stormkit/http_test.go
@@ -3,6 +3,7 @@ package stormkit
 import (
 	"testing"
 	"time"
+	"net/http"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -27,3 +28,44 @@ func TestRequest(t *testing.T) {
 	assert.Nil(t, err)
 	assert.Equal(t, "GET", r.Method)
 }
+
+func TestRequestGet(t *testing.T) {
+	viperInit()
+	Config()
+
+	r, err := Get("a")
+
+	assert.Equal(t, http.MethodGet, r.Method)
+	assert.Nil(t, err)
+}
+
+func TestRequestPost(t *testing.T) {
+	viperInit()
+	Config()
+
+	r, err := Post("a", nil)
+
+	assert.Equal(t, http.MethodPost, r.Method)
+	assert.Nil(t, err)
+}
+
+func TestRequestPut(t *testing.T) {
+	viperInit()
+	Config()
+
+	r, err := Put("a", nil)
+
+	assert.Equal(t, http.MethodPut, r.Method)
+	assert.Nil(t, err)
+}
+
+func TestRequestDelete(t *testing.T) {
+	viperInit()
+	Config()
+
+	r, err := Delete("a", nil)
+
+	assert.Equal(t, http.MethodDelete, r.Method)
+	assert.Nil(t, err)
+}
+
