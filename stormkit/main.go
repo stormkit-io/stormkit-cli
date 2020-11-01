053d3b52decf46bb99364d80dbdcba85d1a9c0a2
giuliobosco <giuliobva@gmail.com>
2020-11-01 17:46:44 +0100

    add stormkit.useHTTPS

--- stormkit/main.go
+++ stormkit/main.go
@@ -10,6 +10,7 @@ const (
 	serverString = "app.server" // is the key of server configuration
 	bearerTokenString = "app.bearer_token" // is the key of bearer token configuration
 	clientTimeoutString = "app.client_timeout" // is the timeout of the http client
+	useHTTPSString = "app.https" // is the flag for use https
 )
 
 // server is the address to the server
@@ -18,11 +19,14 @@ var server string
 var bearerToken string
 // clientTimeout is the timeout of the http client
 var clientTimeout time.Duration
+// useHTTPS is the flag for use https in http requests
+var useHTTPS bool
 
 // Config configure the system for queries via viper (config file)
 func Config() {
 	server = viper.GetString(serverString)
 	bearerToken = viper.GetString(bearerTokenString)
 	clientTimeout = time.Duration(viper.GetInt64(clientTimeoutString))
+	useHTTPS = viper.GetBool(useHTTPSString)
 }
 
--- stormkit/main_test.go
+++ stormkit/main_test.go
@@ -9,16 +9,25 @@ import (
 )
 
 func TestConfig(t *testing.T) {
+	// setting data in viper
 	expectedServer := "aaaa"
 	viper.Set(serverString, expectedServer)
+
 	expectedBearerToken := "bbbbb"
 	viper.Set(bearerTokenString, expectedBearerToken)
+
 	expectedClientTimeout := 5000
 	viper.Set(clientTimeoutString, expectedClientTimeout)
 
+	expectedUseHTTPS := true
+	viper.Set(useHTTPSString, expectedUseHTTPS)
+
+	// run Config
 	Config()
 
+	// check Config runned correctly
 	assert.Equal(t, expectedServer, server)
 	assert.Equal(t, expectedBearerToken, bearerToken)
 	assert.Equal(t, time.Duration(expectedClientTimeout), clientTimeout)
+	assert.Equal(t, expectedUseHTTPS, useHTTPS)
 }
