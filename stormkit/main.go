1064c0eb9bad295b6dc14599482ac99f410f4c77
giuliobosco <giuliobva@gmail.com>
2020-11-02 11:24:53 +0100

    add stormkit.GetEngineAppID(),SetEngineAppID(a)

--- stormkit/main.go
+++ stormkit/main.go
@@ -11,6 +11,7 @@ const (
 	bearerTokenString = "app.bearer_token" // is the key of bearer token configuration
 	clientTimeoutString = "app.client_timeout" // is the timeout of the http client
 	useHTTPSString = "app.https" // is the flag for use https
+	engineAppIDString = "app.engine.app_id" // is the place for store the app_id
 )
 
 // server is the address to the server
@@ -21,6 +22,8 @@ var bearerToken string
 var clientTimeout time.Duration
 // useHTTPS is the flag for use https in http requests
 var useHTTPS bool
+// engineAppID is the place for store the active app_id
+var engineAppID string
 
 // Config configure the system for queries via viper (config file)
 func Config() {
@@ -28,5 +31,16 @@ func Config() {
 	bearerToken = viper.GetString(bearerTokenString)
 	clientTimeout = time.Duration(viper.GetInt64(clientTimeoutString))
 	useHTTPS = viper.GetBool(useHTTPSString)
+	engineAppID = viper.GetString(engineAppIDString)
 }
 
+// GetEngineAppID return the value of engineAppID
+func GetEngineAppID() string {
+	return engineAppID
+}
+
+// SetEngineAppID set the value in engineAppID and in the stormkit file
+func SetEngineAppID(a string) {
+	engineAppID = a
+	viper.Set(engineAppIDString, a)
+}
--- stormkit/main_test.go
+++ stormkit/main_test.go
@@ -12,6 +12,7 @@ const expectedServer = "aaaa"
 const expectedBearerToken = "bbbb"
 const expectedClientTimeout = 5000
 const expectedUseHTTPS = true
+const expectedEngineAppID = "1010101010"
 
 // viperInit initialize viper for testing
 func viperInit() {
@@ -19,6 +20,7 @@ func viperInit() {
 	viper.Set(bearerTokenString, expectedBearerToken)
 	viper.Set(clientTimeoutString, expectedClientTimeout)
 	viper.Set(useHTTPSString, expectedUseHTTPS)
+	viper.Set(engineAppIDString, expectedEngineAppID)
 }
 
 func TestConfig(t *testing.T) {
@@ -32,4 +34,23 @@ func TestConfig(t *testing.T) {
 	assert.Equal(t, expectedBearerToken, bearerToken)
 	assert.Equal(t, time.Duration(expectedClientTimeout), clientTimeout)
 	assert.Equal(t, expectedUseHTTPS, useHTTPS)
+	assert.Equal(t, expectedEngineAppID, engineAppID)
+}
+
+// TestEngineAppID runs a sequence of manipulation on engineAppID
+func TestEngineAppID(t *testing.T) {
+	viperInit()
+
+	Config()
+
+	assert.Equal(t, expectedEngineAppID, engineAppID)
+	assert.Equal(t, expectedEngineAppID, GetEngineAppID())
+	
+	localEngineAppID := "test"
+
+	SetEngineAppID(localEngineAppID)
+
+	assert.Equal(t, localEngineAppID, engineAppID)
+	assert.Equal(t, localEngineAppID, GetEngineAppID())
+	assert.Equal(t, localEngineAppID, viper.Get(engineAppIDString))
 }
