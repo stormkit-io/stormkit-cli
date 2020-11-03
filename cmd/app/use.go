ab3b64c669731d70eedf8149d62efcb5c1a714c0
giuliobosco <giuliobva@gmail.com>
2020-11-03 12:33:07 +0100

    fix app use - app_id

--- cmd/app/use.go
+++ cmd/app/use.go
@@ -2,7 +2,6 @@ package app
 
 import (
 	"errors"
-	"strconv"
 
 	"github.com/spf13/cobra"
 	"github.com/stormkit-io/stormkit-cli/api"
@@ -12,33 +11,19 @@ import (
 // useCmd represents the use command
 var useCmd = &cobra.Command{
 	Use:   "use",
-	Short: "Select an application to work on",
-	Long: `Select an application where to work on, via his repo name, id or index.
-Via his repo name:
+	Short: "Chose an application via app id",
+	Long: `Choose an application by providing the application id. The chosen application will be cached and will be used as the default application for subsequent commands.
 
-$ stormkit-cli app use <repo_name>
+Example: 
 
-Via his id:
+$ stormkit-cli app use <app_id>
 
-$ stormkit-cli app use --app-id <app_id>
-
-Via his index:
-
-$ stormkit-cli app ls -n
-0 github/myuser/my-project
-$ stormkit-cli app use 0
-
-This command is useful when you are not in the directory of your repository.
-It keeps his memory in the config file, at: app.engine.app_id.
-Be carefull this wouldn't work if you are in the direcotry of another 
-stormkit project.`,
+Please note that this command will have no effect in case it is executed from a directory that contains a "stormkit.config.yml" file. The config file takes precedence over the "app use" command.`,
 	RunE: runAppUse,
 }
 
 func init() {
 	appCmd.AddCommand(useCmd)
-
-	useCmd.Flags().StringP("app-id", "a", "", "ID of the app to use")
 }
 
 func runAppUse(cmd *cobra.Command, args []string) error {
@@ -48,46 +33,18 @@ func runAppUse(cmd *cobra.Command, args []string) error {
 		return err
 	}
 
-	// Check if using app-id flag
-	appID, err := cmd.Flags().GetString("app-id")
-	if err != nil {
-		return err
-	}
-	if len(appID) > 0 {
-		for _, a := range apps.Apps {
-			if a.ID == appID {
-				stormkit.SetEngineAppID(a.ID)
-				return nil
-			}
-		}
-
-		return errors.New("no app found")
-	}
-
 	// check if arguments are present
 	if len(args) == 0 {
 		return errors.New("not enought arguments")
 	}
 
-	// using repo name or index
-	index, err := strconv.Atoi(args[0])
-	if err != nil {
-		for _, a := range apps.Apps {
-			if a.Repo == args[0] {
-				stormkit.SetEngineAppID(a.ID)
-				return nil
-			}
+	// check if app exists
+	for _, a := range apps.Apps {
+		if a.ID == args[0] {
+			stormkit.SetEngineAppID(a.ID)
+			return nil
 		}
-
-		return errors.New("app not found")
 	}
 
-	// checking index
-	if index >= len(apps.Apps) {
-		return errors.New("index too large")
-	}
-
-	// using index
-	stormkit.SetEngineAppID(apps.Apps[index].ID)
-	return nil
+	return errors.New("app not found")
 }
--- cmd/app/use_test.go
+++ cmd/app/use_test.go
@@ -29,43 +29,28 @@ func runAppUseInit() (*httptest.Server, *cobra.Command) {
 	stormkit.Config()
 
 	cmd := cobra.Command{}
-	cmd.Flags().String("app-id", "", "")
 
 	return s, &cmd
 }
 
-func TestRunAppUseNoFlag(t *testing.T) {
-	s, _ := runAppUseInit()
-	defer s.Close()
+func TestRunAppUseNotServer(t *testing.T) {
+	stormkit.Config()
 
 	cmd := cobra.Command{}
 	args := []string{}
 	err := runAppUse(&cmd, args)
 
-	assert.Equal(t, "flag accessed but not defined: app-id", err.Error())
+	assert.Equal(t, "Get http:///apps: http: no Host in request URL", err.Error())
 }
 
-func TestRunAppUseAppId(t *testing.T) {
+func TestRunAppUseNotFound(t *testing.T) {
 	s, cmd := runAppUseInit()
 	defer s.Close()
 
-	cmd.Flags().Set("app-id", expectedApps.Apps[0].ID)
-	args := []string{}
+	args := []string{"1"}
 	err := runAppUse(cmd, args)
 
-	assert.Nil(t, err)
-	assert.Equal(t, expectedApps.Apps[0].ID, viper.GetString("app.engine.app_id"))
-}
-
-func TestRunAppUseAppIDNotFound(t *testing.T) {
-	s, cmd := runAppUseInit()
-	defer s.Close()
-
-	cmd.Flags().Set("app-id", "1")
-	args := []string{}
-	err := runAppUse(cmd, args)
-
-	assert.Equal(t, "no app found", err.Error())
+	assert.Equal(t, "app not found", err.Error())
 }
 
 func TestRunAppUseNotEnoughtArgs(t *testing.T) {
@@ -78,42 +63,11 @@ func TestRunAppUseNotEnoughtArgs(t *testing.T) {
 	assert.Equal(t, "not enought arguments", err.Error())
 }
 
-func TestRunAppUseRepo(t *testing.T) {
-	s, cmd := runAppUseInit()
-	defer s.Close()
-
-	args := []string{expectedApps.Apps[0].Repo}
-	err := runAppUse(cmd, args)
-
-	assert.Nil(t, err)
-	assert.Equal(t, expectedApps.Apps[0].ID, viper.GetString("app.engine.app_id"))
-}
-
-func TestRunAppUseRepoNotFound(t *testing.T) {
-	s, cmd := runAppUseInit()
-	defer s.Close()
-
-	args := []string{"a"}
-	err := runAppUse(cmd, args)
-
-	assert.Equal(t, "app not found", err.Error())
-}
-
-func TestRunAppUseIndexTooLarge(t *testing.T) {
-	s, cmd := runAppUseInit()
-	defer s.Close()
-
-	args := []string{"2"}
-	err := runAppUse(cmd, args)
-
-	assert.Equal(t, "index too large", err.Error())
-}
-
 func TestRunAppUse(t *testing.T) {
 	s, cmd := runAppUseInit()
 	defer s.Close()
 
-	args := []string{"0"}
+	args := []string{expectedApps.Apps[0].ID}
 	err := runAppUse(cmd, args)
 
 	assert.Nil(t, err)
