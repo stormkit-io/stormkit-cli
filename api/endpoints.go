856bf8d2b664eefafc918eac4b6547b1e003db90
giuliobosco <giuliobva@gmail.com>
2020-11-18 06:45:37 +0100

    add api/endpoint - DeployAPI

--- api/endpoints.go
+++ api/endpoints.go
@@ -6,3 +6,6 @@ const GetAppsAPI = "/apps"
 // DeployByIDapi is the api string formatter for Sprintf(), first argument
 // app id, second argument deploy id
 const DeployByIDapi = "/app/%s/deploy/%s"
+
+// DeployAPI is the api string
+const DeployAPI = "/app/deploy"
