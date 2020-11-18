ec1ee4c718a75c9c12cd8fbac762cb342e98763f
giuliobosco <giuliobva@gmail.com>
2020-11-18 09:35:43 +0100

    add model/env - Mock data

--- model/env.go
+++ model/env.go
@@ -21,3 +21,26 @@ type Env struct {
 type EnvsArray struct {
 	Envs []Env `json:"envs"`
 }
+
+// MockEnv for run tests
+var MockEnv = Env{
+	ID:          "1234",
+	AppID:       "1235",
+	Env:         "env",
+	Branch:      "branch",
+	AutoPublish: true,
+}
+
+// MockEnvsArray for run tests
+var MockEnvsArray = EnvsArray{
+	Envs: []Env{
+		MockEnv,
+		Env{
+			ID:          "1236",
+			AppID:       "1237",
+			Env:         "env2",
+			Branch:      "branch2",
+			AutoPublish: false,
+		},
+	},
+}
