a579c8628a3d07b459cf38f8600e861cfcff74eb
giuliobosco <giuliobva@gmail.com>
2020-11-18 09:14:53 +0100

    add model/env

---
+++ model/env.go
@@ -0,0 +1,18 @@
+package model
+
+// Env model rappresent an environemt of an application
+type Env struct {
+	ID     string `json:"id"`
+	AppID  string `json:"appId"`
+	Env    string `json:"env"`
+	Branch string `json:"master"`
+	Build  struct {
+		Entry string `json:"entry"`
+		Cmd   string `json:"cmd"`
+	}
+	AutoPublish bool   `json:"autoPublish"`
+	LastDeploy  Deploy `json:"lastDeploy"`
+	Domain      struct {
+		Verified bool `json:"verified"`
+	}
+}
