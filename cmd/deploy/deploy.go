03816dbef80ea4b7d23a884380ddf66741b3e2d8
giuliobosco <giuliobva@gmail.com>
2020-11-18 11:44:48 +0100

    rm cmd/* - init comments

--- cmd/deploy/deploy.go
+++ cmd/deploy/deploy.go
@@ -20,16 +20,6 @@ var deployCmd = &cobra.Command{
 
 func init() {
 	cmd.GetRootCmd().AddCommand(deployCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// deployCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// deployCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
 func runDeploy(cmd *cobra.Command, args []string) error {
--- cmd/log/log.go
+++ cmd/log/log.go
@@ -19,16 +19,6 @@ var logCmd = &cobra.Command{
 
 func init() {
 	cmd.GetRootCmd().AddCommand(logCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// logCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// logCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
 func runLog(cmd *cobra.Command, args []string) error {
