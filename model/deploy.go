4dfcc15c8e44e6cfcabc51fd463c77f9e32c8393
Giulio Bosco <giuliobva@gmail.com>
2021-01-05 11:23:33 +0100

    add watch option to deploy command (#11)
    * fix api/APIEndpoints & API
    
    * fix commands help args
    
    * add model/env
    
    * add api/endpoints - EnvsAPI
    
    * add model/env - Mock data
    
    * add api/env
    
    * add cmd/deploy - interactive
    
    * fix api/env - endpoint
    
    * fix cmd/deploy - interactive flag testing
    
    * fix cmd/deploy - error handling
    
    * add cmd/deploy - branch select
    
    * refactor cmd/deploy
    
    * add utils/git_test - TestGitBranchesNames
    
    * add cmd/deploy - TestDeployInteractiveNoServer
    
    * add cmd/deploy - runEnvPrompt tests
    
    * fix stormkit - RepoPath
    
    * add cmd/deploy - runBranchPrompt testing
    
    * fix cmd/deploy - deployInteractive() test
    
    * add model/Deploy - LogDifference()
    
    * fix model/deploy - LogDifference() run
    
    * add deploy - watch
    
    * add cmd/deploy - interactive
    
    * fix cmd/deploy - interactive flag testing
    
    * fix cmd/deploy - error handling
    
    * add cmd/deploy - branch select
    
    * refactor cmd/deploy
    
    * add cmd/deploy - TestDeployInteractiveNoServer
    
    * add model/Deploy - LogDifference()
    
    * fix model/deploy - LogDifference() run
    
    * add deploy - watch

--- model/deploy.go
+++ model/deploy.go
@@ -1,5 +1,9 @@
 package model
 
+import (
+	"fmt"
+)
+
 // Deploy data of application
 type Deploy struct {
 	ID                string `json:"id"`
@@ -17,6 +21,56 @@ type Deploy struct {
 	IsRunning         bool   `json:"isRunning"`
 	Preview           string `json:"preview"`
 	Logs              []Log  `json:"logs"`
+}
+
+// LastLog returns the latest log of the deploy
+func (d *Deploy) LastLog() *Log {
+	if len(d.Logs) == 0 {
+		return nil
+	}
+
+	return &d.Logs[len(d.Logs)-1]
+}
+
+// LogDifference retuns the difference of the logs with the one given as arguemnt
+func (d *Deploy) LogDifference(newD *Deploy) (string, error) {
+	// cannot compare differents deploys logs
+	if d.ID != "" && d.ID != newD.ID {
+		return "", fmt.Errorf("cannot compare different deploys")
+	}
+
+	// if no logs in new deploy return empty
+	if len(newD.Logs) == 0 {
+		return "", nil
+	}
+
+	var oldLogMessage, newLogMessage, s string
+
+	// read last log from old deploy
+	if l := d.LastLog(); l != nil {
+		oldLogMessage = l.Message
+	}
+	// read latest readed log from new deploy (via old deploy log)
+	if len(d.Logs) == 0 {
+		//		newLogMessage = newD.Logs[0].Message
+	} else {
+		newLogMessage = newD.Logs[len(d.Logs)-1].Message
+	}
+
+	// append new pieces of latest readed log
+	if len(newLogMessage) > len(oldLogMessage) {
+		s += newLogMessage[len(oldLogMessage):]
+	}
+
+	// append all new logs
+	if len(newD.Logs) > len(d.Logs) {
+		for i := len(d.Logs); i < len(newD.Logs); i++ {
+			s += "\n" + newD.Logs[i].Message
+		}
+		s += "\n"
+	}
+
+	return s, nil
 }
 
 // SingleDeploy is struct containing a Deploy
